7f0d69054feb16699afe3720245e9287cf647d79
Jose Paiva <josedaniel@jdpaiva.me>
2020-11-17 18:24:59 +0000

    fixing error when showing accents

--- db_connect.php
+++ db_connect.php
@@ -1,15 +1,13 @@
 <?php
-  // $host = 'localhost:8889';
   $host = 'localhost';
   $port = 8889;
   $user = 'root';
   $password = 'root';
   $db = 'library';
 
-  // $connect = new mysqli($host, $user, $password, $db);
-  // $connect = mysqli_connect($host, $user, $password, $db);
   $connect = mysqli_init();
   $success = mysqli_real_connect($connect, $host, $user, $password, $db, $port);
+  mysqli_set_charset($connect, 'utf8');
   if(!$connect) {
     die("ERROR: Couldn't connect to database " . mysqli_connect_error());
   }
